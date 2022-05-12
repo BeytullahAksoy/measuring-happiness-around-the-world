bd09f7e5f3d41fa96e6624f05d7d53b750f59035
beytullahaksoy <beytullah557@gmail.com>
2022-05-12 21:11:44 +0300

    feat: flask template update

--- app.py
+++ app.py
@@ -1,26 +1,52 @@
 from flask import Flask, render_template, request
+import pandas as pd
+import json
+import plotly
+import plotly.express as px
+
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET', 'POST'])
+
+
+@app.route('/')
 def index():
-    print(request.method)
+    return render_template('index.html')
+
+
+@app.route('/chart1')
+def chart1():
+    df = pd.DataFrame({
+        "Fruit": ["Angry", "Disgust", "Fear","Happy","Neutral","Sad","Surprise"],
+        "Amount": [4, 100, 200,300,200,500,1],
+        "City": ["SF", "SF", "SF","SF", "SF", "SF","SF"]
+    })
+
+    fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    header = "Fruit in North America"
+    description = """
+    A academic study of the number of apples, oranges and bananas in the cities of
+    San Francisco and Montreal would probably not come up with this chart.
+    """
+    return render_template('analyze.html', graphJSON=graphJSON, header=header, description=description)
+
+
+@app.route("/chart2", methods=['GET', 'POST'])
+def chart2():
     if request.method == 'POST':
-        if request.form.get('Encrypt') == 'Encrypt':
-            # pass
-            print("Encrypted")
-        elif request.form.get('Decrypt') == 'Decrypt':
-            # pass # do something else
-            print("Decrypted")
+        if request.form.get('action1') == 'VALUE1':
+            pass  # do something
+        elif request.form.get('action2') == 'VALUE2':
+            pass  # do something else
         else:
-            # pass # unknown
-            return render_template("index.html")
+            pass  # unknown
     elif request.method == 'GET':
-        # return render_template("index.html")
-        print("No Post Back Call")
+        return render_template('dataset.html')
+
     return render_template("index.html")
 
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
